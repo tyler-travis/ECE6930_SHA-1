a04f2c14ecdda803cd3536d9eb6c7eb6e51cf6a6
Justin <justin.n.cox@gmail.com>
2016-01-18 13:39:32 -0700

    Added comments and function ideas

--- main.cpp
+++ main.cpp
@@ -1,18 +1,43 @@
 //********************************************************************
-//	Title:
-//	Class:
-//	Author(s):
-//	Date:
+//	Title: SHA-1 Software Implementation (main.cpp)
+//	Class: ECE 6760 Hardware Security
+//	Author(s): Tyler Travis & Justin Cox
+//	Date: 1/19/2016
 //********************************************************************
 
 //********************************************************************
 //	Function Prototypes
 //********************************************************************
-
+void prepMessage(void);
+void shaIteration(unsigned int a, unsigned int b, unsigned int c, unsigned int d, unsigned int e);
+void printSHA(void); 
 //********************************************************************
 //	Main Function
 //********************************************************************
+int main(){
 
+	//Initial Buffer Values
+	unsigned int h0 = 0x67452301
+	unsigned int h1 = 0xEFCDAB89
+	unsigned int h2 = 0x98BADCFE
+	unsigned int h3 = 0x10325476
+	unsigned int h4 = 0xC3D2E1F0
+
+	//End program
+	return 0;
+}
 //********************************************************************
 //	Function Definitions
 //********************************************************************
+
+void prepMessage(void){
+
+}
+
+void shaIteration(unsigned int a, unsigned int b, unsigned int c, unsigned int d, unsigned int e){
+
+}
+
+void printSHA(void){
+
+}
