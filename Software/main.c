ae73d6576044d01f5ceb97ca4fb01baf37333ead
Tyler Travis <tyler.travis@aggiemail.usu.edu>
2016-01-22 10:16:25 -0700

    Added some TODO stuff at the top of main.c and fixed some mixed indentations

--- Software/main.c
+++ Software/main.c
@@ -6,6 +6,22 @@
 //********************************************************************
 
 //********************************************************************
+//	Some Ideas for future optimization/readability
+//********************************************************************
+//
+//  1. Use a struct to carry all the information besides the message
+//     and chunks array, that way we can condense the information
+//     better.
+//  2. Is there a way to make the padding section smaller? Looking at
+//     some code on line implementing SHA-1 in C shows a lot less code
+//     for that.
+//  3. Look into using some of the Intel Intrinsic functions.
+//  4. Optimize the SHA_Iteration function to better use the boolean
+//     expressions.
+//
+//********************************************************************
+
+//********************************************************************
 //	Pre-processing
 //********************************************************************
 
@@ -21,7 +37,7 @@
 void SHA1(char* message, uint32_t hash_buffer[5], uint32_t message_size);
 void prepMessage(char* message, uint32_t chunks[][16], uint64_t message_size_bits, uint32_t numChunks, uint32_t leftOverBits, uint8_t addChunk);
 void shaIteration(uint32_t hash_buffer[5], uint32_t chunk[16]);
-void printSHA(uint32_t hash_buffer[5]); 
+void printSHA(uint32_t hash_buffer[5]);
 
 uint32_t rotl(uint32_t value, uint16_t shift);
 
@@ -35,7 +51,7 @@ int main(int argc, char** argv)
     // We will by applying the SHA-1 algorithm to this message
     FILE *fp;
     fp = fopen(argv[1], "r");
-    
+
     // Determine size of the file
     fseek(fp, 0, SEEK_END);
     long fsize = ftell(fp);
@@ -57,10 +73,10 @@ int main(int argc, char** argv)
     printf("\n\nMessage: %s\n", message);
     printSHA(hash_buffer);
 
-	//End program
+    //End program
     fclose(fp);
     free(message);
-	return 0;
+    return 0;
 }
 
 //********************************************************************
@@ -83,34 +99,34 @@ void SHA1(char* message, uint32_t hash_buffer[5], uint32_t message_size)
     uint8_t addChunk = 0;
 
     if(leftOverBits < 448){
-    	addChunk = 1;
+        addChunk = 1;
     }
     else{
-    	addChunk = 2;
+        addChunk = 2;
     }
 
     uint64_t number_of_chunks = (message_size_bytes/64) + addChunk;
-    
+
     uint16_t i;
     uint16_t j;
 
-    // Initialize the chunks array 
+    // Initialize the chunks array
     uint32_t chunks[number_of_chunks][16];
 
     // Prep the message into 512-bit chunks (16 32-bit words)
     prepMessage(message, chunks, message_size_bits, number_of_chunks, leftOverBits, addChunk);
 
-	//################################
-	//	FOR DEBUGGING
-	//################################
+    //################################
+    //	FOR DEBUGGING
+    //################################
     printf("Message AFTER prep: \n");
     for(i = 0; i < number_of_chunks; i++){
-    	for(j = 0; j < 16; j++){
-    		printf("%08X", chunks[i][j]);
-    	}
+        for(j = 0; j < 16; j++){
+            printf("%08X", chunks[i][j]);
+        }
     }
-	//################################
-	//################################    
+    //################################
+    //################################
 
     // This manipulates the bytes as defined by SHA-1
     for(i = 0; i < number_of_chunks; ++i)
@@ -121,163 +137,163 @@ void SHA1(char* message, uint32_t hash_buffer[5], uint32_t message_size)
 
 void prepMessage(char* message, uint32_t chunks[][16], uint64_t message_size_bits, uint32_t numChunks, uint32_t leftOverBits, uint8_t addChunk)
 {
-	//512 bits = 64 bytes
-	//		   = 16 words 
-	//448 bits = 56 bytes
-	//		   = 14 words
-	//64 bits = 8 bytes
-	//		  = 2 words
-	//32 bits = 4 bytes
+    //512 bits = 64 bytes
+    //		   = 16 words
+    //448 bits = 56 bytes
+    //		   = 14 words
+    //64 bits = 8 bytes
+    //		  = 2 words
+    //32 bits = 4 bytes
 
-	uint16_t numBytesPadding = 0;
-	
-	uint16_t i = 0;
-	uint16_t j = 0;
+    uint16_t numBytesPadding = 0;
 
-	uint8_t offset = 24;
+    uint16_t i = 0;
+    uint16_t j = 0;
 
-	//Split message into 512 bit chunks excluding last chunk
-	for(i = 0; i < (numChunks - addChunk); i++){
-		for(j = 0; j < 16; j++){
-			chunks[i][j] = message[(0*j)+(i*64)]<<24 | message[(1*j)+(i*64)]<<16 | message[(2*j)+(i*64)]<<8 | message[(3*j)+(i*64)];
-			//memcpy(chunks[i], message + ((j*4)+(i*64)), sizeof(uint32_t));
-		}
-	}
+    uint8_t offset = 24;
+
+    //Split message into 512 bit chunks excluding last chunk
+    for(i = 0; i < (numChunks - addChunk); i++){
+        for(j = 0; j < 16; j++){
+            chunks[i][j] = message[(0*j)+(i*64)]<<24 | message[(1*j)+(i*64)]<<16 | message[(2*j)+(i*64)]<<8 | message[(3*j)+(i*64)];
+            //memcpy(chunks[i], message + ((j*4)+(i*64)), sizeof(uint32_t));
+        }
+    }
 
 
-	//Fill in last chunk of message bits
-	j = 0;
-	for(i = 0; i < (leftOverBits/8); i++){
-		//Set word to 0x00000000
-		if(offset == 24){
-			chunks[numChunks-addChunk][j] = 0;			
-		}
+    //Fill in last chunk of message bits
+    j = 0;
+    for(i = 0; i < (leftOverBits/8); i++){
+        //Set word to 0x00000000
+        if(offset == 24){
+            chunks[numChunks-addChunk][j] = 0;
+        }
 
-		//Add byte to word in correct position
-		chunks[numChunks-addChunk][j] |= (((message[i + ((numChunks-addChunk)*64)]) | 0x00000000) << offset);
+        //Add byte to word in correct position
+        chunks[numChunks-addChunk][j] |= (((message[i + ((numChunks-addChunk)*64)]) | 0x00000000) << offset);
 
-		//Reset offset for new word, else decrease offset to next byte position
-		if(offset == 0){
-			offset = 24;
-			j++;
-		}
-		else{
-			offset = offset - 8;	
-		}
-		 
-	}
+        //Reset offset for new word, else decrease offset to next byte position
+        if(offset == 0){
+            offset = 24;
+            j++;
+        }
+        else{
+            offset = offset - 8;
+        }
 
-	//--------------------------------
-	//	PADDING
-	//--------------------------------
-	printf("Offset: %d \n", offset);
-	printf("Byte number %d \n", j);
+    }
 
-	//Different methods for leftOverBits >= 448 and leftOverBits < 448
-	switch(addChunk)
-	{
-		case 1:
-		{
-			//Calculate bytes of padding
-			numBytesPadding = 56 - (leftOverBits/8);
-			printf("# padding: %d \n", numBytesPadding);
+    //--------------------------------
+    //	PADDING
+    //--------------------------------
+    printf("Offset: %d \n", offset);
+    printf("Byte number %d \n", j);
 
-			for(i = 0; i < numBytesPadding; i++){
-				if(i == 0){
-					//First byte is 0x80
-					chunks[numChunks-1][j] |= (0x80 << offset);
-				}
-				else
-				{
-					chunks[numChunks-1][j] &= (0x00 << offset);	
-				}
+    //Different methods for leftOverBits >= 448 and leftOverBits < 448
+    switch(addChunk)
+    {
+        case 1:
+            {
+                //Calculate bytes of padding
+                numBytesPadding = 56 - (leftOverBits/8);
+                printf("# padding: %d \n", numBytesPadding);
 
-				if(offset == 0){
-					offset = 24;
-					j++;
-				}
-				else{
-					offset = offset - 8;	
-				}
-			}
+                for(i = 0; i < numBytesPadding; i++){
+                    if(i == 0){
+                        //First byte is 0x80
+                        chunks[numChunks-1][j] |= (0x80 << offset);
+                    }
+                    else
+                    {
+                        chunks[numChunks-1][j] &= (0x00 << offset);
+                    }
 
-			break;
-			
-		} //end of case 1
-		case 2:
-		{
-			//Calculate bytes of padding
-			numBytesPadding = 64 - (leftOverBits/8);
+                    if(offset == 0){
+                        offset = 24;
+                        j++;
+                    }
+                    else{
+                        offset = offset - 8;
+                    }
+                }
 
-			for(i = 0; i < numBytesPadding; i++){
-				if(i == 0){
-					//First byte is 0x80
-					chunks[numChunks-2][j] |= (0x80 << offset);
-				}
-				else
-				{
-					chunks[numChunks-2][j] &= (0x00 << offset);	
-				}
+                break;
 
-				if(offset == 0){
-					offset = 24;
-					j++;
-				}
-				else{
-					offset = offset - 8;	
-				}
-			}
+            } //end of case 1
+        case 2:
+            {
+                //Calculate bytes of padding
+                numBytesPadding = 64 - (leftOverBits/8);
 
-			//Add 448 bits to last chunk
-			for(i = 0; i < 14; i++){
-				chunks[numChunks-1][i] = 0;
-			}
+                for(i = 0; i < numBytesPadding; i++){
+                    if(i == 0){
+                        //First byte is 0x80
+                        chunks[numChunks-2][j] |= (0x80 << offset);
+                    }
+                    else
+                    {
+                        chunks[numChunks-2][j] &= (0x00 << offset);
+                    }
 
-			break;
+                    if(offset == 0){
+                        offset = 24;
+                        j++;
+                    }
+                    else{
+                        offset = offset - 8;
+                    }
+                }
 
-		} //end of case 2
-	} //end of switch
+                //Add 448 bits to last chunk
+                for(i = 0; i < 14; i++){
+                    chunks[numChunks-1][i] = 0;
+                }
 
-	//--------------------------------
-	//	APPEND 64-bit message length
-	//--------------------------------
+                break;
 
-	//MSW
-	chunks[numChunks-1][14] = message_size_bits >> 32;
-	//LSW
-	chunks[numChunks-1][15] = message_size_bits & 0x00000000FFFFFFFF; 
-	 
+            } //end of case 2
+    } //end of switch
 
-	//################################
-	//	FOR DEBUGGING
-	//################################
-	
-	//PRINT THE ORIGINAL MESSAGE
-	printf("\n");
-	printf("The message size in bytes: %d \n", (int)(message_size_bits/8));
-	printf("The message size in bits: %d \n", (int)message_size_bits);
-	printf("# of leftover bits: %d \n", (int)leftOverBits);
-	
+    //--------------------------------
+    //	APPEND 64-bit message length
+    //--------------------------------
 
-	for(i = 0; i < (message_size_bits/8); i++){
-		printf("%c", message[i]);
-	}
+    //MSW
+    chunks[numChunks-1][14] = message_size_bits >> 32;
+    //LSW
+    chunks[numChunks-1][15] = message_size_bits & 0x00000000FFFFFFFF;
 
-	//PRINT THE DATA OF ALL THE CHUNKS BY WORDS
-	printf("\n\n");
-	printf("Number of chunks: %d \n", numChunks);
-	
-	for(i = 0; i < numChunks; i++){
-		for(j = 0; j < 16; j++){
-			printf("%08X", chunks[i][j]);
-		}
-		printf("\n\n");
-	}
 
-	printf("\n\n");
+    //################################
+    //	FOR DEBUGGING
+    //################################
 
-	//################################
-	//################################
+    //PRINT THE ORIGINAL MESSAGE
+    printf("\n");
+    printf("The message size in bytes: %d \n", (int)(message_size_bits/8));
+    printf("The message size in bits: %d \n", (int)message_size_bits);
+    printf("# of leftover bits: %d \n", (int)leftOverBits);
+
+
+    for(i = 0; i < (message_size_bits/8); i++){
+        printf("%c", message[i]);
+    }
+
+    //PRINT THE DATA OF ALL THE CHUNKS BY WORDS
+    printf("\n\n");
+    printf("Number of chunks: %d \n", numChunks);
+
+    for(i = 0; i < numChunks; i++){
+        for(j = 0; j < 16; j++){
+            printf("%08X", chunks[i][j]);
+        }
+        printf("\n\n");
+    }
+
+    printf("\n\n");
+
+    //################################
+    //################################
 
 }
 
@@ -336,7 +352,7 @@ void shaIteration(uint32_t hash_buffer[5], uint32_t chunk[16])
             f = b ^ c ^ d;
             k = 0xCA62C1D6;
         }
-        
+
         // Store the values in the correct location
         temp = rotl(a, 5) + f + e + k + w[i];
         e = d;
